c478c85c06da03a8d3809e45df7227d912fd3aec
unknown <quentindevial19@gmail.com>
2022-03-16 17:15:47 +0100

    feat: first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<script setup lang="ts">import type { Page } from '@/interfaces';
+
+defineProps<{
+    page: Page
+}>()
+
+const emit = defineEmits<{
+    (e: 'navigate', page: Page): void
+}>()
+
+</script>
+
+<template>
+    <header class="px-20 d-flex flex-row align-items-center">
+        <a href="#" class="d-flex flex-row align-items-center mr-20">
+            <img src="../assets/logo.svg" />
+            <span class="logo">Dyma</span>
+        </a>
+        <ul class="d-flex flex-row flex-fill">
+            <li class="mr-10">
+                <a
+                    :class="{ active: page === 'Boutique' }"
+                    @click="emit('navigate', 'Boutique')"
+                >Boutique</a>
+            </li>
+            <li>
+                <a :class="{ active: page === 'Admin' }" @click="emit('navigate', 'Admin')">Admin</a>
+            </li>
+        </ul>
+        <ul class="d-flex flex-row">
+            <li class="mr-10">
+                <a href="#">Inscription</a>
+            </li>
+            <li>
+                <a href="#">Connexion</a>
+            </li>
+        </ul>
+    </header>
+</template>
+
+<style lang="scss" scoped>
+header {
+    background-color: var(--primary-1);
+    a {
+        color: var(--text-primary-color);
+        img {
+            width: 20px;
+            margin-right: 5px;
+        }
+        .logo {
+            font-weight: 700;
+            font-size: 20px;
+        }
+    }
+    a.active {
+        text-decoration: underline;
+    }
+}
+</style>
---
+++ src/features/boutique/components/Shop/ShopProductList.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import type { ProductInterface } from '@/interfaces';
+import ShopProduct from './ShopProduct.vue';
+
+defineProps<{
+    products: ProductInterface[]
+}>()
+
+const emit = defineEmits<{
+    (e: 'addProductToCart', productId: string): void
+}>()
+
+</script>
+
+<template>
+    <div class="grid p-20">
+        <ShopProduct
+            @add-product-to-cart="emit('addProductToCart', $event)"
+            v-for="product of products"
+            :key="product._id"
+            :product="product"
+        />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use '@/assets/scss/mixins' as m;
+.grid {
+    display: grid;
+    grid-template-columns: 1fr;
+    @include m.md {
+        grid-template-columns: 1fr 1fr;
+    }
+    @include m.lg {
+        grid-template-columns: 1fr 1fr 1fr;
+    }
+    @include m.xl {
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+    }
+    grid-auto-rows: 400px;
+    gap: 20px;
+}
+</style>
